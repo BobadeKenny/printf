2da829603c62d3da4ba2a2cc4d6642475282e7f1
Kenny <kennygbeminiyi@gmail.com>
2022-04-18 22:59:38 +0100

    header corrections

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN
+#define MAIN
 
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+#endif
