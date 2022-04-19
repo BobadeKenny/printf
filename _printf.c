96a9191b100d61b2f27bf3356bcf2a83542f8bc0
marybanire <baniremary@gmail.com>
2022-04-19 18:00:53 +0100

    printf.c file

--- _printf.c
+++ _printf.c
@@ -9,38 +9,32 @@
  */
 int _printf(const char *format, ...)
 {
-int (*pfunc)(va_list, flags_t *);
-const char *p;
-va_list arguments;
-flags_t flags = {0, 0, 0};
+va_list list;
+unsigned int i;
+int j, res;
+	
 
-register int count = 0;
-
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
+if(format == NULL)
+{
 return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = format; *p; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == '%')
-{
-count += _putchar('%');
-continue;
 }
-while (get_flag(*p, &flags))
-p++;
-pfunc = get_print(*p);
-count += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
-} else
-count += _putchar(*p);
+va_start(list, format);
+for (j = 0; format[j] != '\0'; j++)
+{
+if (format[j] == '%')
+{
+switch(format[j + 1])
+{
+case 'c':
+i = va_arg(list, int);
+res = _putchar(i);
+break;
+case 's':
+res = _print_str(va_arg(list, char *));
+break;
 }
-_putchar(-1);
-va_end(arguments);
-return (count);
+}
+}
+va_end(list);
+return(res);
 }
